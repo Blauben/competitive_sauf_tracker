b057e6a7d68755c84b58c1dd918c0da890580d51
Moritz <ge95van@mytum.de>
2022-12-10 11:23:35 +0100

    Added first layout idea

--- lib/main_features/drink_selector/body/drink_selector.dart
+++ lib/main_features/drink_selector/body/drink_selector.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sauf_tracker/main_features/drink_selector/widgets/drink_selector_item.dart';
 
+
+
 class DrinkSelectorBody extends StatelessWidget {
   const DrinkSelectorBody({Key? key}) : super(key: key);
 
@@ -8,34 +10,30 @@ class DrinkSelectorBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        Container(
-          margin: EdgeInsets.only(left: 15, right: 15),
-          height: 200,
-          child: DrinkSelectorItem(title: "Bier", icon: Icon(Icons.sports_bar, size: 100),),
+        _buildCardItem(title: "Bier", icon: Icon(Icons.sports_bar, size: 100,)),
+        _buildCardItem(title: "Wein", icon: Icon(Icons.wine_bar, size: 100,)),
 
-        ),
-        Container(
-          margin: EdgeInsets.only(left: 15, right: 15),
-          height: 200,
-          child: DrinkSelectorItem(title: "Wein", icon: Icon(Icons.wine_bar, size: 100,),),
+        _buildCardItem(title: "Whisky", icon: Icon(Icons.liquor, size: 100,)),
 
-        ),
-
-        Container(
-          margin: EdgeInsets.only(left: 15, right: 15),
-          height: 200,
-          child: DrinkSelectorItem(title: "Whisky", icon: Icon(Icons.liquor, size: 100,),),
-
-        ),
-
-        Container(
-          margin: EdgeInsets.only(left: 15, right: 15),
-          height: 200,
-          child: DrinkSelectorItem(title: "Cocktail", icon: Icon(Icons.local_bar_sharp, size: 100,),),
-
-        ),
+        _buildCardItem(title: "Cocktail", icon: Icon(Icons.local_bar_sharp, size: 100,))
 
       ],
     );
   }
+
+  Container _buildCardItem({required String title, required Icon icon}) {
+    return Container(
+      margin: EdgeInsets.only(left: 15, right: 15),
+      height: 200,
+      child: DrinkSelectorItem(title: title, icon: icon,),
+
+    );
+  }
+
+  void _showBody(int index) {
+
+  }
+
+
 }
+
